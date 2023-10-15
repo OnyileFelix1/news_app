8ba5643c5fb21feb5dd6fe23253fc0781ba4a4ae
Onyile Bassey Felix <basseyonyile20044@gmail.com>
2023-10-15 16:38:06 +0100

    Add files via upload

---
+++ news-app-master/news-app-master/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { NewsList } from './components'
+
+function App() {
+  return (
+    <div className="App">
+      <NewsList />
+    </div>
+  );
+}
+
+export default App;
